2a16a0343a19db9336265fc297789a85c152b522
Balazs Balazs <balazs.balazs80@gmail.com>
2014-04-05 09:51:23 +0100

    Add the login screen

---
+++ app/src/main/java/com/krazyrobot/robobook/LoginActivity.java
@@ -0,0 +1,44 @@
+package com.krazyrobot.robobook;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.text.Editable;
+import android.text.TextUtils;
+import android.text.TextWatcher;
+import android.widget.Button;
+import android.widget.EditText;
+
+
+public class LoginActivity extends ActionBarActivity {
+
+    private EditText mEmailEditText;
+    private Button mLoginButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        mLoginButton = (Button)findViewById(R.id.login_button);
+        mEmailEditText = (EditText)findViewById(R.id.email_edittext);
+        mEmailEditText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i2, int i3) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i2, int i3) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                if (TextUtils.isEmpty(editable.toString())) {
+                    mLoginButton.setEnabled(false);
+                } else {
+                    mLoginButton.setEnabled(true);
+                }
+            }
+        });
+    }
+}
