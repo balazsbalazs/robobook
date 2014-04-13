f6c67e33f197e866bd1564d19359e720b02dfb27
Balazs Balazs <balazs.balazs80@gmail.com>
2014-04-13 10:22:01 +0100

    Add map activity

--- app/src/main/java/com/krazyrobot/robobook/FollowRobotsActivity.java
+++ app/src/main/java/com/krazyrobot/robobook/FollowRobotsActivity.java
@@ -1,7 +1,11 @@
 package com.krazyrobot.robobook;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.ListView;
 
 public class FollowRobotsActivity extends Activity {
@@ -12,5 +16,24 @@ public class FollowRobotsActivity extends Activity {
         ListView listView = (ListView) findViewById(R.id.follow_list);
         FollowAdapter adapter = new FollowAdapter(this);
         listView.setAdapter(adapter);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.follow_friends_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.next:
+                startActivity(new Intent(this, MapActivity.class));
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+
     }
 }
---
+++ app/src/main/java/com/krazyrobot/robobook/MapActivity.java
@@ -0,0 +1,13 @@
+package com.krazyrobot.robobook;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MapActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.map_activity);
+    }
+}
