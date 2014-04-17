a90b6c631bc3c0c2166fc384a3ef55bb30b8883a
Balazs Balazs <balazs.balazs80@gmail.com>
2014-04-17 10:30:29 +0100

    Following a robot makes it selected (with tick)

--- app/src/main/java/com/krazyrobot/robobook/FollowAdapter.java
+++ app/src/main/java/com/krazyrobot/robobook/FollowAdapter.java
@@ -8,6 +8,9 @@ import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class FollowAdapter extends BaseAdapter {
     private final static Robot[] ROBOTS = {
             new Robot("Terminator", R.drawable.terminator),
@@ -22,6 +25,7 @@ public class FollowAdapter extends BaseAdapter {
             new Robot("Sonny", R.drawable.sonny)
     };
     private final LayoutInflater mInflater;
+    private final Set<Robot> mFollowedRobots = new HashSet<Robot>();
 
     public FollowAdapter(Context context) {
         mInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
@@ -47,9 +51,29 @@ public class FollowAdapter extends BaseAdapter {
         View itemView = mInflater.inflate(R.layout.follow_item, parent, false);
         ImageView img = (ImageView) itemView.findViewById(R.id.avatar);
         TextView name = (TextView) itemView.findViewById(R.id.name);
-        Robot robot = ROBOTS[position];
+        final Robot robot = ROBOTS[position];
         img.setImageResource(robot.getmImgRes());
         name.setText(robot.getmName());
+        final View followButton = itemView.findViewById(R.id.follow_btn);
+        final View followTick = itemView.findViewById(R.id.followed_tick);
+        followButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mFollowedRobots.add(robot);
+                refreshButtons(robot, followButton, followTick);
+            }
+        });
+        refreshButtons(robot, followButton, followTick);
         return itemView;
+    }
+
+    private void refreshButtons(Robot robot, View followButton, View followTick) {
+        if (mFollowedRobots.contains(robot)) {
+            followButton.setVisibility(View.GONE);
+            followTick.setVisibility(View.VISIBLE);
+        } else {
+            followButton.setVisibility(View.VISIBLE);
+            followTick.setVisibility(View.GONE);
+        }
     }
 }
