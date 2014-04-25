b9f5dd3e2cb70b168b505053b9ee09553bd5a166
Balazs Balazs <balazs.balazs80@gmail.com>
2014-04-25 14:01:30 +0100

    Updated welcome screen - nicer layout

--- app/src/main/java/com/krazyrobot/robobook/WelcomeActivity.java
+++ app/src/main/java/com/krazyrobot/robobook/WelcomeActivity.java
@@ -1,30 +1,29 @@
 package com.krazyrobot.robobook;
 
 import android.content.Intent;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.View;
-import android.widget.Button;
 
 
 public class WelcomeActivity extends ActionBarActivity {
 
-    private Button mLoginButton;
-    private Button mSignButton;
+    private View mLoginButton;
+    private View mSignButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
 
-        mLoginButton = (Button)findViewById(R.id.login_button);
+        mLoginButton = findViewById(R.id.login_button);
         mLoginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(WelcomeActivity.this, LoginActivity.class));
             }
         });
-        mSignButton = (Button)findViewById(R.id.signin_button);
+        mSignButton = findViewById(R.id.signin_button);
         mSignButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
